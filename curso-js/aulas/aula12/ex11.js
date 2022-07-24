228a489473532a620d441bb856ca64d4363e333d
eunaotenhocriatividade <andreicruz1203@gmail.com>
2022-07-24 10:34:48 -0300

    criei novos exercicios

---
+++ curso-js/aulas/aula12/ex11.js
@@ -0,0 +1,8 @@
+var idade = 71
+if (16 <=  idade && idade < 18 || idade >= 65){
+    console.log('Voto opcional')
+}else if (idade >= 18) {
+    console.log('Voto obrigatorio')
+ }else{
+    console.log('Voto negado')
+ }
