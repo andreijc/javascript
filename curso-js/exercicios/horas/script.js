b0c40cac49792174297070e0a1d383bb4dfa2e1a
eunaotenhocriatividade <andreicruz1203@gmail.com>
2022-07-27 17:12:23 -0300

    atualizei os site

--- curso-js/exercicios/horas/script.js
+++ curso-js/exercicios/horas/script.js
@@ -1,2 +1,11 @@
 var agora = new Date()
 var hora = agora.getHours()
+var img = document.getElementById('img')
+var msg = document.querySelector('#msg')
+
+hora = 6
+
+if (hora < 12) {
+        img.style.backgroundImage = 'url(imagens/pexels-pixabay-207247.jpg)'
+        msg.innerHTML = '<p>Agora é de manhãn</p>'
+}
