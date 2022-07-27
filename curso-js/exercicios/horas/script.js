59f8ec7e6f1fe16415e29ca52b9e115d5f5a04be
eunaotenhocriatividade <andreicruz1203@gmail.com>
2022-07-27 17:55:08 -0300

    finalizei o exercicio

--- curso-js/exercicios/horas/script.js
+++ curso-js/exercicios/horas/script.js
@@ -2,12 +2,22 @@ var agora = new Date()
 var hora = agora.getHours()
 var img = document.getElementById('img')
 var msg = document.querySelector('#msg')
+var corpo = document.querySelector('.corpo')
 
-hora = 6
+hora = 18
 
 
 
 if (hora < 12) {
-        img.style.background = 'url(imagens/pexels-pixabay-207247.jpg)'
+        img.style.backgroundImage = 'url(estilos/imagens/manha.jpg)'
         msg.innerHTML = '<p>Agora é de manhã</p>' 
+        corpo.style.backgroundColor= 'var(--manha)'
+} else if (hora >= 12 && hora < 18) {
+        img.style.backgroundImage = 'url(estilos/imagens/tarde.jpg)'
+        msg.innerHTML = '<p>Agora é de tarde</p>'
+        corpo.style.backgroundColor = 'var(--tarde)'
+} else if ( 18 <= hora ) {
+        img.style.backgroundImage = 'url(estilos/imagens/noite.jpg)'
+        msg.innerHTML = '<p>Agora é de noite</p>'
+        corpo.style.backgroundColor = 'var(--noite)'
 }
