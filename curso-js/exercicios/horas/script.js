e0348e77dc628eb58f931a6b1ff553cc0a718574
eunaotenhocriatividade <andreicruz1203@gmail.com>
2022-07-27 17:24:53 -0300

    ajustei as imagens

--- curso-js/exercicios/horas/script.js
+++ curso-js/exercicios/horas/script.js
@@ -5,7 +5,9 @@ var msg = document.querySelector('#msg')
 
 hora = 6
 
+
+
 if (hora < 12) {
-        img.style.backgroundImage = 'url(imagens/pexels-pixabay-207247.jpg)'
-        msg.innerHTML = '<p>Agora é de manhãn</p>'
+        img.style.background = 'url(imagens/pexels-pixabay-207247.jpg)'
+        msg.innerHTML = '<p>Agora é de manhã</p>' 
 }
